104d032a36a5e17d10f2a7ab076e24221c3e0635
DanielGorczynski <daniel.gorczynski@rice.edu>
2019-05-28 09:01:46 -0500

    edited notation, removed excess code

--- Median Psi.R
+++ Median Psi.R
@@ -1,7 +1,7 @@
-
-#Function for extracting occurance medians from raw TEAM data
+#Function for extracting occupancy medians from raw TEAM data
+#Resulting Occurence data frame can be inputed into later functions
 Occ <- function(x){
-x[is.na(x)] <- 0
+x[is.na(x)] <- 0 
 x <- subset(x, class == "MAMMALIA")
 Species <- x$bin
 Species <- unique(Species) #Obtain list of species in community
@@ -18,55 +18,17 @@ for (p in Species){ #run loop to fill in matrix one vector (species) at a time
 Year <- cbind(Year, Psivector)
 }
 
-rownames(Year) <- Year[,1] #Some rearrangement of matrix to give data frame compatible with Gower Distance
+rownames(Year) <- Year[,1] #Some rearrangement of matrix to make data frame compatible with later functions
 Year <-  Year [,-1] 
 colnames(Year) <- Species
 Year <- t(Year)
-Occurance <- as.data.frame(Year)
-Occurance <- cbind( "Species" = rownames(Occurance), Occurance) 
-rownames(Occurance) <- NULL
-Occurance <- as.data.frame(Occurance)
-return(Occurance)
+Occurence <- as.data.frame(Year)
+Occurence <- cbind( "Species" = rownames(Occurence), Occurence) 
+rownames(Occurence) <- NULL
+Occurence <- as.data.frame(Occurence)
+return(Occurence)
 }
 
-Occurance <- Occ(VBdata_psi)
+Occurence <- Occ(VBdata_psi)
 
-
-
-
-
-
-#Test
-for (i in 2007:2014){
-  Mams <- subset(VBdata_psi, bin == "Procyon lotor")
-  Psi1000 <- subset(Mams, year == i)
-  MedPsi <- median(Psi1000$psi)
-  Psivector[[i-2006]] <- MedPsi
-}
-
-#Rearrange Occurance table to get change over time
-a <- as.data.frame(Occurance$`2007`)
-a <- cbind(a, Occurance$Species)
-a <- cbind(a, 2007)
-colnames(a) <- c("Occurance","Species", "Year")
-
-b <- as.data.frame(Occurance$`2014`)
-b <- cbind(b, Occurance$Species)
-b <- cbind(b, 2014)
-colnames(b) <- c("Occurance","Species", "Year")
-
-data <- rbind(data,b)
-
-#for removing abundant species
-dat <- data
-dat <- subset(dat, Species != "Mazama temama")
-
-#For getting just carnivores
-carn <- dat
-carn <- subset(carn, Species == "Eira barbara" | Species == "Leopardus pardalis" | Species == "Puma concolor" 
-                 | Species == "Puma yagouaroundi" | Species == "Leopardus wiedii" | Species == "Panthera onca")
-
-#Obtain ggplot
-ggplot(data = carn, aes(x = Year, y = Occurance, group = Species, color = Species))+
-  geom_line() 
 
