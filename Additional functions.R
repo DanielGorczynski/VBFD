379d8d35987bd77dec0e64fe3e765c4ac6c06f71
DanielGorczynski <daniel.gorczynski@rice.edu>
2019-05-28 09:57:35 -0500

    Rearranged from Functional Diversity script

--- Additional functions.R
+++ Additional functions.R
@@ -64,3 +64,64 @@ mean(Null_vector)
 var(Null_vector)
 quantile(Null_vector, probs = seq(0.025,.975,1))
 quantile(Null_vector, probs = seq(.975,1))
+
+
+##Functional Redundancy NULL model
+Func.Red.NULL <- function(x,y,ft,i){
+  
+  Community_Div<-vector("numeric")
+  Pres.Abs<- filter(x,x[[y]] > 0)
+  Pres.Abs.red <- Pres.Abs[sample(nrow(Pres.Abs),i,replace = FALSE),]
+  Community<-Pres.Abs$Species
+  Comm.Funct<-ft[ft[["Species"]] %in% Community,]
+  rownames(Comm.Funct)<-Comm.Funct$Species
+  Comm.Funct$Species<-NULL
+  Pres.Abs.red <- as.data.frame(Pres.Abs.red[c('Species',y )])
+  rownames(Pres.Abs.red)<-Pres.Abs.red$Species
+  Pres.Abs.red$Species<-NULL
+  
+  res.pca<-PCA(Comm.Funct, graph = F) #PCA
+  Funct.Coord<-res.pca$ind$coord
+  Funct.Coord.5<-as.data.frame(Funct.Coord)
+  #Funct.Coord.4 <- Funct.Coord.5[c(1:4)]
+  Funct.Coord.3<-Funct.Coord.5[c(1:3)] #parse out first 3 dimmensions of PCA only
+  #Funct.Coord.2 <- Funct.Coord.5[c(1:2)]
+  Func.Dist.Matrix<-daisy(Funct.Coord.3, metric = "gower") #Gower on first 3 PCA dimmensions
+  Func.Dendrogram<-hclust(Func.Dist.Matrix, method = "average") #Construct Dendrogram
+  Dend.Dist.Mat <- cophenetic(Func.Dendrogram) #Dendrogram difference between all species
+  Dend.Dist.Mat <- as.matrix(Dend.Dist.Mat)
+  Dend.Dist.df <- as.data.frame(Dend.Dist.Mat)
+  Avg.Func.Div <- data.frame(row.names = rownames(Dend.Dist.df), Means=rowMeans(Dend.Dist.df))
+  #Avg.Func.Div<-Avg.Func.Div[rownames(Avg.Func.Div) %in% rownames(Pres.Abs.red),]
+  
+  Sigma<-merge(Avg.Func.Div,Pres.Abs.red,by="row.names") #incorporate funct. div in calculation table
+  colnames(Sigma) <- c("Species","Means","Abundance")
+  #Sigma$Means <- sample(Sigma$Means, replace = FALSE)
+  #Sigma["Influence"]<-(Sigma$Means*Sigma$Abundance) #product of funct. div and change in occurence
+  Sigma$Means <- sample(Sigma$Means, replace = FALSE)
+  Sigma["Influence"]<-(Sigma$Means*(Sigma$Abundance/sum(Sigma$Abundance)))
+  FD<-(sum(Sigma$Influence)) #Sums species influences for Turnover
+  return(FD)
+}
+
+#Get functional redundancy null model for a year (mean, min and max FD of all sub-community sizes)
+mean.FR <- vector("numeric")
+min.FR <- vector("numeric")
+max.FR <- vector("numeric")
+for (i in 2:22) {
+  Red_vector <- replicate(1000, Func.Red.NULL(Occurance,"2014",Volcan.Barva.Traits,i))
+  mean.FR[i] <- mean(Red_vector)
+  min.FR[i] <- quantile(Red_vector, probs = seq(0.025,.975,1))
+  max.FR[i] <- quantile(Red_vector, probs = seq(.975,1))
+}
+cat(mean.FR, sep = "\n")
+cat(min.FR, sep = "\n")
+cat(max.FR, sep = "\n")
+
+Red_vector_null <- replicate(1000, Func.Red.NULL(Occurance,"2011",Volcan.Barva.Traits,2))
+print(Red_vector_null)
+mean(Red_vector_null)
+var(Red_vector_null)
+quantile(Red_vector_null, probs = seq(0.025,.975,1))
+quantile(Red_vector_null, probs = seq(.975,1))
+
--- Figures.R
+++ Figures.R
@@ -1,3 +1,21 @@
+##Plot Functional Diversity Over Time with Functional Diversity Null Model confidence interval
+ggplot()+
+  geom_ribbon(data = Annual.Functional.Diversity, aes(x = Year, ymin = Func.Null.Low, ymax = Func.Null.High), fill = "grey90") +
+  geom_point(data = Annual.Functional.Diversity, aes(x = Year, y = Func.Div, col = "Functional Diversity"))+
+  geom_line(data = Annual.Functional.Diversity, aes(x = Year, y = Func.Div, col = "Functional Diversity")) +
+  #geom_errorbar(data = Annual.Functional.Diversity, aes(x = Year, ymin = Abund.Null.Low, ymax = Abund.Null.High), width = .2) +
+  #geom_point(data = Dpw.Values.10000, aes(x = Year, y = Null.Dpw, col = "Null Model Turnover", group = Park)) +
+  #geom_line(data = Dpw.Values.10000, aes(x = Year, y = Null.Dpw, col = "Null Model Turnover", group = Park)) +
+  scale_colour_manual("", 
+                      breaks = c("Functional Diversity", "Null Model"),
+                      values = c("black", "grey70"))+
+  theme_classic()+
+  theme(axis.text = element_text(size = rel(1.5)))+
+  labs( title = "Mammalian Functional Diversity in Volcan Barva over time", y = "Functional Diversity")+
+  ylim(0.27, 0.34)
+
+
+
 ##Plot Functional Diversity Model Predictions with confidence interval
 library(ggplot2)
 ggplot()+
@@ -12,3 +30,21 @@ ggplot()+
   theme(axis.text = element_text(size = rel(1.5)))+
   labs( title = "Predicted Functional Diversity", y = "Functional Diversity")+
   ylim(0.313, 0.325)
+
+
+
+##Plot Functional Redundancy, NEEDS EXPLANATION OF VARIABLES SINCE DATA TABLE IS NOT PUT TOGETHER IN FUNCTIONS SCRIPT
+ggplot()+
+  #geom_ribbon(data = Func.Redund, aes(x = Species, ymin = Null.Min, ymax = Null.Max), fill = "grey70") +
+  geom_point(data = Functional.Redundancy, aes(x = Species.Loss, y = Loss, col = factor(Year)))+
+  geom_line(data = Functional.Redundancy, aes(x = Species.Loss, y = Loss, col = factor(Year))) +
+  #geom_point(data = Func.Redund, aes(x = Species, y = Null.Mean)) +
+  #geom_line(data = Func.Redund, aes(x = Species, y = Null.Mean)) +
+  scale_colour_manual("", 
+                      #breaks = c("Measured Functional Diversity", "Null Model Diversity"),
+                      values = c("red","orange","yellow","green","blue","darkblue","purple","black"))+
+  theme_classic()+
+  theme(axis.text = element_text(size = rel(1.5)))+
+  theme(axis.title = element_text(size = rel(1.5)))+
+  labs( title = "Functional Redundancy in Volcan Barva", y = "Functional Diversity", x = "Species Lost")+
+  ylim(0.295,0.325)
