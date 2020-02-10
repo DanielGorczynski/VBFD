98f448cfffac65a336ab80f4e381f6e96f32c0f6
DanielGorczynski <daniel.gorczynski@rice.edu>
2020-02-10 16:09:03 -0600

    More figure editting, removed old FD file

--- Figures.R
+++ Figures.R
@@ -11,7 +11,7 @@ FDis.plot <- ggplot()+
                       values = c("#31a354"))+
   theme_classic()+
   theme(axis.text = element_text(size = rel(.9)))+
-  labs( title = "Mammalian Functional Dispersion over time", y = "FDis")+
+  labs( title = "Annual mammalian functional dispersion", y = "FDis")+
   ylim(2.1, 2.3)+
   theme(legend.position = "none", axis.title.x =element_blank())
   
@@ -24,7 +24,7 @@ Avg.Occ.plot <- ggplot()+
                       values = c("#006d2c"))+
   theme_classic()+
   theme(axis.text = element_text(size = rel(.9)))+
-  labs( title = "Average Mammalian Occupancy over time", y = "Mean Occ.")+
+  labs( title = "Annual average mammalian occupancy", y = "Mean Occ.")+
   ylim(.11, .2)+
   theme(legend.position = "none",axis.title.x =element_blank())
 
@@ -37,7 +37,7 @@ Prec.plot <- ggplot()+
                       values = c("#edf8e9"))+
   theme_classic()+
   theme(axis.text = element_text(size = rel(.9)))+
-  labs( title = "Total Precipitation over time", y = "Annual Prec.")+
+  labs( title = "Annual precipitation", y = "Prec.")+
   ylim(1100, 2100)+
   theme(legend.position = "none", axis.title.x =element_blank())
 
@@ -49,7 +49,7 @@ Can.gap.plot <- ggplot()+
                       values = c("#c7e9c0"))+
   theme_classic()+
   theme(axis.text = element_text(size = rel(.9)))+
-  labs( title = "Annual Canopy Gap Emergence", y = "Area")+
+  labs( title = "Annual canopy gap emergence", y = "Area")+
   ylim(0, 4)+
   theme(legend.position = "none", axis.title.x =element_blank())
 
@@ -61,7 +61,7 @@ Frag.plot <- ggplot()+
                       values = c("#a1d99b"))+
   theme_classic()+
   theme(axis.text = element_text(size = rel(.9)))+
-  labs( title = "Mean Fragment size over time", y = "Area")+
+  labs( title = "Mean fragment size in ZOI", y = "Area")+
   ylim(99, 106)+
   theme(legend.position = "none", axis.title.x =element_blank())
 
@@ -73,19 +73,44 @@ Defor.plot <- ggplot()+
                       values = c("#74c476"))+
   theme_classic()+
   theme(axis.text = element_text(size = rel(.9)))+
-  labs( title = "Annual Forest Loss in ZOI", y = "Area")+
+  labs( title = "Annual forest loss in ZOI", y = "Area")+
   ylim(600, 2800)+
   theme(legend.position = "none", axis.title.x =element_blank())
 
 ##Combine trends over time into single figure
-ggarrange(Prec.plot,Can.gap.plot,Frag.plot,Defor.plot, FDis.plot,Avg.Occ.plot, ncol = 1, label.x = "Year")
+
+ggarrange(Prec.plot,Can.gap.plot,Frag.plot,Defor.plot, FDis.plot,Avg.Occ.plot, ncol = 1, align = "hv", label.x = "Year")
+
 ##Export as a 5x10 PDF
+
+##Plot relationship between fragment size and average occupancy over time
+ggplot()+
+  geom_point(data = Volcan.Barva.Analysis, aes(x = FM_ZOI_90, y = Occupancy.Avg))+
+  theme_classic()+
+  geom_smooth(data = Volcan.Barva.Analysis, 
+              aes(x = FM_ZOI_90, y = Occupancy.Avg), 
+              method = "lm", 
+              color = "black")+
+  theme(axis.text = element_text(size = rel(1.25)))+
+  labs(x = "Mean fragment size (hectares)", y = "Average species occupancy")+
+  annotate("text",
+           x = 105,
+           y = .21, 
+           label = "italic(p) == 0.00368",
+           size = rel(5),
+           parse = TRUE)+
+  annotate("text",
+           x = 105,
+           y = .20, 
+           label = "italic(R^2) == 0.7425",
+           size = rel(5),
+           parse = TRUE)
 
 ##Plot Functional Redundancy, NEEDS EXPLANATION OF VARIABLES SINCE DATA TABLE IS NOT PUT TOGETHER IN FUNCTIONS SCRIPT
 ggplot()+
   #geom_ribbon(data = Func.Redund, aes(x = Species, ymin = Null.Min, ymax = Null.Max), fill = "grey70") +
-  geom_point(data = Functional.Redundancy, aes(x = Species.Loss, y = Loss, col = factor(Year)))+
-  geom_line(data = Functional.Redundancy, aes(x = Species.Loss, y = Loss, col = factor(Year))) +
+  geom_point(data = Functional.Redundancy, aes(x = Species.Loss, y = Fdis, col = factor(Year)))+
+  geom_line(data = Functional.Redundancy, aes(x = Species.Loss, y = Fdis, col = factor(Year))) +
   #geom_point(data = Func.Redund, aes(x = Species, y = Null.Mean)) +
   #geom_line(data = Func.Redund, aes(x = Species, y = Null.Mean)) +
   scale_colour_manual("", 
@@ -96,6 +121,157 @@ ggplot()+
   theme(axis.title = element_text(size = rel(1.5)))+
   labs( title = "Functional Redundancy in Volcan Barva", y = "Functional Diversity", x = "Species Lost")
   #ylim()
+
+
+##Plot temporal trends in functional traits
+
+##Body Mass
+BM.plot <- ggplot()+
+  geom_point(data = VB.FD.CWM, aes(x = Year, y = Body.Mass))+
+  theme_classic()+
+  geom_smooth(data = VB.FD.CWM, 
+              aes(x = Year, y = Body.Mass), 
+              method = "lm", 
+              color = "black")+
+  theme(axis.text = element_text(size = rel(1.25)), 
+        axis.title = element_text(size = rel(1.5)))+
+  labs(x = "Year", y = "Log. Average Body Mass")+
+  annotate("text",
+           x = 2008,
+           y = 1.3, 
+           label = "italic(p) == 0.0544",
+           size = rel(5),
+           parse = TRUE)+
+  annotate("text",
+           x = 2008,
+           y = 1.27, 
+           label = "italic(R^2) == 0.4012",
+           size = rel(5),
+           parse = TRUE)
+
+##Diet
+Diet.plot <- ggplot()+
+  geom_point(data = VB.FD.CWM, aes(x = Year, y = Diet))+
+  theme_classic()+
+  geom_smooth(data = VB.FD.CWM, 
+              aes(x = Year, y = Diet), 
+              method = "lm", 
+              color = "black")+
+  theme(axis.text = element_text(size = rel(1.25)), 
+        axis.title = element_text(size = rel(1.5)))+
+  labs(x = "Year", y = "Average Diet Rank")+
+  annotate("text",
+           x = 2013,
+           y = 5.3, 
+           label = "italic(p) == 0.0978",
+           size = rel(5),
+           parse = TRUE)+
+  annotate("text",
+           x = 2013,
+           y = 5.25, 
+           label = "italic(R^2) == 0.2885",
+           size = rel(5),
+           parse = TRUE)
+
+##Social Group
+Soc.plot <- ggplot()+
+  geom_point(data = VB.FD.CWM, aes(x = Year, y = Social.Group.Size))+
+  theme_classic()+
+  geom_smooth(data = VB.FD.CWM, 
+              aes(x = Year, y = Social.Group.Size), 
+              method = "lm", 
+              color = "black")+
+  theme(axis.text = element_text(size = rel(1.25)), 
+        axis.title = element_text(size = rel(1.5)))+
+  labs(x = "Year", y = "Average Social Group Size")+
+  annotate("text",
+           x = 2008,
+           y = 1.7, 
+           label = "italic(p) == 0.00107",
+           size = rel(5),
+           parse = TRUE)+
+  annotate("text",
+           x = 2008,
+           y = 1.68, 
+           label = "italic(R^2) == 0.8276",
+           size = rel(5),
+           parse = TRUE)
+
+##Habitat
+Hab.plot <- ggplot()+
+  geom_point(data = VB.FD.CWM, aes(x = Year, y =Habitat))+
+  theme_classic()+
+  geom_smooth(data = VB.FD.CWM, 
+              aes(x = Year, y = Habitat), 
+              method = "lm", 
+              color = "black")+
+  theme(axis.text = element_text(size = rel(1.25)), 
+        axis.title = element_text(size = rel(1.5)))+
+  labs(x = "Year", y = "Average Habitat Rank")+
+  annotate("text",
+           x = 2013,
+           y = 2.2, 
+           label = "italic(p) == 0.1957",
+           size = rel(5),
+           parse = TRUE)+
+  annotate("text",
+           x = 2013,
+           y = 2.19, 
+           label = "italic(R^2) == 0.1378",
+           size = rel(5),
+           parse = TRUE)
+
+##Activity Period
+Act.plot <- ggplot()+
+  geom_point(data = VB.FD.CWM, aes(x = Year, y =Activity.Period))+
+  theme_classic()+
+  geom_smooth(data = VB.FD.CWM, 
+              aes(x = Year, y = Activity.Period), 
+              method = "lm", 
+              color = "black")+
+  theme(axis.text = element_text(size = rel(1.25)), 
+        axis.title = element_text(size = rel(1.5)))+
+  labs(x = "Year", y = "Average Activity Period Rank")+
+  annotate("text",
+           x = 2013,
+           y = 3.35, 
+           label = "italic(p) == 0.000296",
+           size = rel(5),
+           parse = TRUE)+
+  annotate("text",
+           x = 2013,
+           y = 3.3, 
+           label = "italic(R^2) == 0.8869",
+           size = rel(5),
+           parse = TRUE)
+
+##Litter Size
+Lit.plot <- ggplot()+
+  geom_point(data = VB.FD.CWM, aes(x = Year, y = Avg..Litter.Size))+
+  theme_classic()+
+  geom_smooth(data = VB.FD.CWM, 
+              aes(x = Year, y = Avg..Litter.Size), 
+              method = "lm", 
+              color = "black")+
+  theme(axis.text = element_text(size = rel(1.25)), 
+        axis.title = element_text(size = rel(1.5)))+
+  labs(x = "Year", y = "Average Litter Size")+
+  annotate("text",
+           x = 2013,
+           y = 1.9, 
+           label = "italic(p) == 0.06982",
+           size = rel(5),
+           parse = TRUE)+
+  annotate("text",
+           x = 2013,
+           y = 1.87, 
+           label = "italic(R^2) == 0.355",
+           size = rel(5),
+           parse = TRUE)
+
+##Combine regressions into a single figure
+
+ggarrange(BM.plot,Diet.plot,Soc.plot,Hab.plot, Act.plot,Lit.plot, ncol = 3, nrow = 2, align = "hv")
 
 
 ###OLD ITERATIONS FOR POTENTIAL USE LATER
