299417980c9717164d86db46cd8e8f94cd2ef2c6
DanielGorczynski <daniel.gorczynski@rice.edu>
2020-03-04 13:06:25 -0600

    Removed excess linear models from old iterations

--- Linear Modeling.R
+++ Linear Modeling.R
@@ -1,19 +1,10 @@
+
+
 ##Test for temporal trends
-x <- lm(Occupancy.Avg ~ Year, data = Volcan.Barva.Analysis)
+
+x <- lm(Fdis ~ Year, data = VBAnalysis_Ordered)
 summary(x)
 
-x <- lm(Fdis ~ Year, data = Volcan.Barva.Analysis)
-summary(x)
-
-##Volcan Barva Occupancy Average Model Selection
-library(MuMIn)
-fitFD <- lm(Fdis~ FL_VB + Var.Temp + FM_ZOI_90 + FL_ZOI, 
-            data=VBAnalysis_Ordered, na.action = "na.fail")
-
-allFD.dredge <- dredge(fitFD, beta=TRUE, evaluate=TRUE, rank="AICc", trace=TRUE, extra=list("confint", "adjR^2"))
-allFD.dredge
-x <- lm(Occupancy.Avg ~ FM_ZOI_90, data = Volcan.Barva.Analysis)
-summary(x)
 
 #Volcan Barva Fdis Model Selection
 library(MuMIn)
@@ -23,11 +14,36 @@ fitFD <- lm(Fdis~ FL_VB + Var.Temp + FM_ZOI_90 + FL_ZOI,
 allFD.dredge <- dredge(fitFD, beta=TRUE, evaluate=TRUE, rank="AICc", trace=TRUE, extra=list("confint", "adjR^2"))
 allFD.dredge
 
-##Test for temporal trends in CWM values
-p <- lm(VB.FD$CWM$Body.Mass ~ Year, data = Volcan.Barva.Analysis)
-p <- lm(VB.FD$CWM$Diet ~ Year, data = Volcan.Barva.Analysis)
-p <- lm(VB.FD$CWM$Social.Group.Size ~ Year, data = Volcan.Barva.Analysis) ##Significant
-p <- lm(VB.FD$CWM$Habitat ~ Year, data = Volcan.Barva.Analysis)
-p <- lm(VB.FD$CWM$Activity.Period ~ Year, data = Volcan.Barva.Analysis) ##Significant
-p <- lm(VB.FD$CWM$Avg..Litter.Size ~ Year, data = Volcan.Barva.Analysis)
-summary(p)
+##Test for temporal trends in traits
+##Make ordered categories for clm ordinal regression
+Ordinal.trait.analysis$Diet <- ordered(Ordinal.trait.analysis$Diet, 
+                                       levels = c("Browser","Browser/Frugivore",
+                                                  "Frugivore","Omnivore","Insectivore","Zoophage","Carnivore"))
+Ordinal.trait.analysis$Social <- ordered(Ordinal.trait.analysis$Social, 
+                                         levels = c("Solitary/pairs","Coalitions",
+                                                    "Family groups","Gregarious"))
+Ordinal.trait.analysis$Habitat <- ordered(Ordinal.trait.analysis$Habitat, 
+                                          levels = c("terrestrial","scansorial"))
+Ordinal.trait.analysis$Activity <- ordered(Ordinal.trait.analysis$Activity, 
+                                           levels = c("Diurnal","Non-restricted",
+                                                      "Crepuscular","Nocturnal/Crepuscular","Nocturnal"))
+
+##linear and ordinal models
+library(ordinal)
+x <- lm(Body.Mass ~ Study.Year, data = Ordinal.trait.analysis, weights = Occupancy)
+x <- clm(Diet~Study.Year, data = Ordinal.trait.analysis, weights = Occupancy)
+x <- clm(Social~Study.Year, data = Ordinal.trait.analysis, weights = Occupancy)
+x <- clm(Habitat~Study.Year, data = Ordinal.trait.analysis, weights = Occupancy)
+x <- clm(Activity~Study.Year, data = Ordinal.trait.analysis, weights = Occupancy)
+x <- lm(Litter ~ Study.Year, data = Ordinal.trait.analysis, weights = Occupancy)
+
+##Test for environmental and anthropogenic predictors of traits
+fitFD <- lm(Litter~ FL_VB + Tot.Prec + FM_ZOI_90 + FL_ZOI, 
+            data=Ordinal.trait.analysis, na.action = "na.fail", weights = Occupancy)
+fitFD <- lm(Body.Mass~ FL_VB + Tot.Prec + FM_ZOI_90 + FL_ZOI, 
+            data=Ordinal.trait.analysis, na.action = "na.fail")
+fitFD <- clm(Diet~ FL_VB + Tot.Prec + FM_ZOI_90 + FL_ZOI, 
+            data=Ordinal.trait.analysis, na.action = "na.fail", weights = Occupancy)
+
+allFD.dredge <- dredge(fitFD, beta=TRUE, evaluate=TRUE, rank="AICc", trace=TRUE, extra=list("confint", "adjR^2"))
+allFD.dredge
