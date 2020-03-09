95ea9670e274cf9a00b093a528d898b73ce2404e
DanielGorczynski <daniel.gorczynski@rice.edu>
2020-03-09 11:34:31 -0500

    Edited linear model

--- Linear Modeling.R
+++ Linear Modeling.R
@@ -8,8 +8,8 @@ summary(x)
 
 #Volcan Barva Fdis Model Selection
 library(MuMIn)
-fitFD <- lm(Fdis~ FL_VB + Var.Temp + FM_ZOI_90 + FL_ZOI, 
-            data=Volcan.Barva.Analysis, na.action = "na.fail")
+fitFD <- lm(Fdis~ FL_VB + Tot.Prec + FM_ZOI_90 + FL_ZOI, 
+            data=VBAnalysis_Ordered, na.action = "na.fail")
 
 allFD.dredge <- dredge(fitFD, beta=TRUE, evaluate=TRUE, rank="AICc", trace=TRUE, extra=list("confint", "adjR^2"))
 allFD.dredge
